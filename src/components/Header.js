5a3deb01b313a4d02c5ab32f4aa758829e33ea4d
Megan Katherine O'Brien <megankob@gmail.com>
2020-03-20 13:43:20 -0400

    Added Header and some notes

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React, { Fragment } from 'react';
+
+//TODO: Add my icons next to my name? Maybe if I am feeling extra
+
+const Header = () => (
+    <Fragment>
+        <header>
+            <div>
+                <h1>
+                    Compound Tracking
+                </h1>
+                <p>Kaleido Code Challenge by Megan Katherine O'Brien</p>
+            </div>
+        </header>
+    </Fragment>
+);
+
+export default Header;
