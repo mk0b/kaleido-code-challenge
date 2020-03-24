f4347dda1239a67981fb164f9e26fc222332c77b
Megan Katherine O'Brien <megankob@gmail.com>
2020-03-23 20:10:41 -0400

    Fleshing out Register Compound form

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './App.css';
 
-//TODO: Make this into a stateful component.
-
 import Header from './Header';
 import RegisterComponent from './RegisterCompound';
 
--- src/components/RegisterCompound.js
+++ src/components/RegisterCompound.js
@@ -1,29 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const RegisterCompound = () => {
+//stateful component that manages compound and well data.
+
+import GridDisplay from './GridDisplay';
+
+class RegisterCompound extends Component {
 
     //convert this to stateful? 
     //Consider using contextAPI
     //add onchange to track form input
     //add value to inputs
 
-    return (
-        <div className="register-compound-container">
-            <button>New Compound</button>
-            <div className="register-compound-modal">
-                <form>
-                    <input 
-                        id="compoundName"
-                        name="compoundName"
-                        type="text"
-                        placeholder="Compound Name"
-                    />
-                    <input />
-                    <button>Cancel</button>
-                </form>
+    constructor(props) {
+        //binding this keyword to this class
+        super(props);
+        //setting state to manage compound and well data
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div className="register-compound-container">
+                <button>New Compound</button>
+                <div className="register-compound-modal">
+                    <form>
+                        <input 
+                            id="plateID"
+                            name="plateID"
+                            type="text"
+                            placeholder="Plate"
+                        />
+                        <input 
+                            id="compoundID"
+                            name="compoundID"
+                            type="text"
+                            placeholder="Compound ID"
+                        />
+                        <input 
+                            id="well"
+                            name="well"
+                            type="text"
+                            placeholder="Well"
+                        />
+                        <button>Cancel</button>
+                    </form>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default RegisterCompound;
