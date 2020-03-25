abb1ac1b64f4c841dff701c6db8ba9bc47313571
Megan Katherine O'Brien <megankob@gmail.com>
2020-03-25 00:07:30 -0400

    Updates

---
+++ precache-manifest.b17a09980edd0983098d26b353071f97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac603792c0aba6e88886c20dd62aa8d8",
+    "url": "/kaleido-code-challenge/index.html"
+  },
+  {
+    "revision": "624aaf841645f1a3322c",
+    "url": "/kaleido-code-challenge/static/css/main.7c10a726.chunk.css"
+  },
+  {
+    "revision": "42951129bc6ebb699d37",
+    "url": "/kaleido-code-challenge/static/js/2.9966f559.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kaleido-code-challenge/static/js/2.9966f559.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "624aaf841645f1a3322c",
+    "url": "/kaleido-code-challenge/static/js/main.3c8ef938.chunk.js"
+  },
+  {
+    "revision": "6c7a0e2c80b595673bc6",
+    "url": "/kaleido-code-challenge/static/js/runtime-main.9db11eef.js"
+  }
+]);
